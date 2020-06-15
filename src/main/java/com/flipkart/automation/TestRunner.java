b739bc31dc19d1cbeb393cd39215bdb0f41b61e7
tejaswini-git94 <66852038+tejaswini-git94@users.noreply.github.com>
2020-06-15 01:31:51 -0500

    Create TestRunner.java

---
+++ src/main/java/com/flipkart/automation/TestRunner.java
@@ -0,0 +1,20 @@
+package com.flipkart.automation;
+
+import java.io.File;
+
+import org.junit.runner.RunWith;
+
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+ features = "src/test/resources/featurefiles"
+ ,glue={"com/flipkart/automation/stepdefs"}, tags={"@searchtest1"},plugin = { "pretty", "html:target/cucumber-reports" },
+ monochrome = true)
+
+public class TestRunnerClass {
+	 
+}
